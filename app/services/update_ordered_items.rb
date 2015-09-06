37e56027dcfbec872192a06ae4fc5faa21613c45
jonathansayer <jon.sayer1@gmail.com>
2015-09-06 09:01:12 +0100

    rewrote the UpdateOrderedItems service to take in Product name instead of Product id

--- app/services/update_ordered_items.rb
+++ app/services/update_ordered_items.rb
@@ -1,14 +1,23 @@
 class UpdateOrderedItems
 
-  def self.add_to_ordered_items product_id
-    product = self.find_item product_id
-    OrderedProduct.create(name:product.name, price:product.price)
+  def self.add_to_ordered_items product_name
+    if self.find_duplicates product_name
+      product = OrderedProduct.find_by name:product_name
+      product.update_attribute(:quantity, product.quantity += 1)
+    else
+      product = self.find_item product_name
+      OrderedProduct.create(name: product.name, price: product.price, quantity: 1)
+    end
+  end
+
+  def self.find_duplicates product_name
+    OrderedProduct.exists?(name:product_name)
   end
 
   private
 
-  def self.find_item product_id
-    Product.find(product_id)
+  def self.find_item product_name
+    Product.find_by name:product_name
   end
 
 end
--- spec/services/update_ordered_items_spec.rb
+++ spec/services/update_ordered_items_spec.rb
@@ -2,15 +2,30 @@ require 'rails_helper'
 
 describe UpdateOrderedItems do
 
-  let(:subject) { UpdateOrderedItems}
+  let(:subject) { UpdateOrderedItems }
 
   it 'the class method add_to_ordered_items must be called with an arguemnt' do
     expect(subject).to respond_to(:find_item)
   end
 
   it 'must add the Product to the OrderedItems table' do
-    product = Product.create(id:3, name:'BirdPrintDress,Black')
-    subject.add_to_ordered_items 3
+    product = Product.create(name:'BirdPrintDress,Black')
+    subject.add_to_ordered_items 'BirdPrintDress,Black'
     expect(OrderedProduct.first.name).to eq 'BirdPrintDress,Black'
+  end
+
+  describe 'finding duplicates' do
+    it 'must be able to find products that have already been purchased' do
+      product = Product.create(name:'BirdPrintDress,Black')
+      subject.add_to_ordered_items 'BirdPrintDress,Black'
+      expect(subject.find_duplicates 'BirdPrintDress,Black').to eq true
+    end
+
+    it 'if a duplicate is found, the quantity is increased by one' do
+      product = Product.create(name:'BirdPrintDress,Black')
+      subject.add_to_ordered_items 'BirdPrintDress,Black'
+      subject.add_to_ordered_items 'BirdPrintDress,Black'
+      expect(OrderedProduct.first.quantity).to eq 2
+    end
   end
 end
