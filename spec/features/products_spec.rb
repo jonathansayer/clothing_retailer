7510b5c3cae5179004931e00c227134e63b0ad5e
jonathansayer <jon.sayer1@gmail.com>
2015-09-06 22:20:57 +0100

    cannnot select an item which is out of order

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -3,17 +3,21 @@ require 'rails_helper'
 feature 'products' do
   context 'when products are available' do
     scenario 'products should be displayed on the home page' do
-      Product.create(name:"Suede Shoes,Blue")
+      Product.create(name:"Suede Shoes,Blue",quantity: 2)
       visit '/'
       expect(page).to have_content("Suede Shoes,Blue")
     end
 
     scenario 'should display the price of each product' do
-      Product.create(name:"Suede Shoes,Blue", price: 42.00)
+      Product.create(name:"Suede Shoes,Blue", price: 42.00, quantity: 2)
       visit '/'
       expect(page).to have_content "£42.00"
     end
 
-  
+    scenario 'should not be able to be purchaed when out of stock' do
+      Product.create(name:"Suede Shoes,Blue", price: 42.00, quantity: 0)
+      visit '/'
+      expect(page).not_to have_button "Add to Cart"
+    end
   end
 end
