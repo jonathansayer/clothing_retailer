3a58c07b2e8f9c940fecb5c23b37859d45613da0
jonathansayer <jon.sayer1@gmail.com>
2015-09-06 14:37:55 +0100

    shows the totals of individual items in shopping cart

--- spec/features/shopping_cart_feature_spec.rb
+++ spec/features/shopping_cart_feature_spec.rb
@@ -8,7 +8,7 @@ feature 'shopping cart' do
     end
 
     scenario 'should be able to add items to the cart' do
-      Product.create(name: "Suede Shoes, Blue")
+      Product.create(name: "Suede Shoes, Blue",price: 42.00, quantity:1)
       visit '/'
       click_button "Suede Shoes, Blue"
       expect(current_path).to eq "/shopping_cart"
@@ -16,7 +16,7 @@ feature 'shopping cart' do
     end
 
     scenario 'should be able to remove items from the cart' do
-      Product.create(name: "FlipFlops,Red")
+      Product.create(name: "FlipFlops,Red",price: 19.00, quantity:1)
       visit '/'
       click_button 'FlipFlops,Red'
       expect(current_path).to eq "/shopping_cart"
@@ -25,14 +25,25 @@ feature 'shopping cart' do
     end
 
     scenario 'should display the price of the individual items' do
-      OrderedProduct.create(name: "AlmondToeCourtShoes,PatentBlack", price: 99.00)
-      OrderedProduct.create(name: "SuedeShoes,Blue", price: 42.00)
-      OrderedProduct.create(name: "LeatherDriverSaddleLoafers,Tan", price:34.00)
+      OrderedProduct.create(name: "AlmondToeCourtShoes,PatentBlack", price: 99.00, quantity:1)
+      OrderedProduct.create(name: "SuedeShoes,Blue", price: 42.00, quantity:1)
+      OrderedProduct.create(name: "LeatherDriverSaddleLoafers,Tan", price:34.00, quantity:1)
       visit '/shopping_cart'
       expect(page).to have_content "£99.00"
       expect(page).to have_content "£42.00"
       expect(page).to have_content "£34.00"
     end
+
+    scenario 'should display totals for individual items ' do
+      OrderedProduct.create(name: "AlmondToeCourtShoes,PatentBlack", price: 99.00, quantity: 2)
+      OrderedProduct.create(name: "SuedeShoes,Blue", price: 42.00, quantity: 3)
+      OrderedProduct.create(name: "LeatherDriverSaddleLoafers,Tan", price:34.00, quantity: 4)
+      visit '/shopping_cart'
+      expect(page).to have_content "£198.00"
+      expect(page).to have_content "£126.00"
+      expect(page).to have_content "£136.00"
+    end
+
 
 
   end
