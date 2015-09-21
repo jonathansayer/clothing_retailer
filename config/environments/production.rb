3a1bc3fcf730e6f7b9832426449d30da16ad3325
jonathansayer <jon.sayer1@gmail.com>
2015-09-21 17:19:11 +0100

    Buttons reformated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.precompile += %w( users.css static_pages.css )
   # Code is not reloaded between requests.
   config.cache_classes = true
 
