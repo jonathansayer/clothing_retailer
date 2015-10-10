c913e4addb834f3c6e97d5003d769a0f5c0c4d9f
jonathansayer <jon.sayer1@gmail.com>
2015-10-10 14:18:52 +0100

    removed last migration and seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name:"Almond Toe Court Shoes, Patent Black",
+              catagory:"Women’s Footwear",
+              price: 99.00,
+              stock: 5,
+              img: "AlmondToeCourtShoes.png" )
+Product.create(name:"Suede Shoes, Blue",
+              catagory:"Women’s Footwear",
+              price: 42.00,
+              stock: 4,
+              img: "SuedeShoesBlue.png" )
+Product.create(name:"Leather Driver Saddle Loafers, Tan",
+              catagory:"Men’s Footwear",
+              price: 34.00,
+              stock: 12,
+              img: "LeatherDriverSaddleLoafersTan.png" )
+Product.create(name:"Flip Flops, Red",
+              catagory:"Men’s Footwear",
+              price: 19.00,
+              stock: 6,
+              img: "FlipFlopsRed.png" )
+Product.create(name:"Flip Flops, Blue",
+              catagory:"Men’s Footwear",
+              price: 19.00,
+              stock: 0,
+              img: "FlipFlopsBlue.png" )
+Product.create(name: "Gold Button Cardigan, Black",
+               catagory: "Women’s Casualwear",
+               price: 167.00,
+               stock: 6,
+               img: "GoldButtonCardiganBlack.png")
+Product.create(name: "Cotton Shorts, Medium Red",
+              catagory: "Women’s Casualwear",
+              price: 30.00,
+              stock: 5,
+              img: "CottonShortsMediumRed.png")
+Product.create(name: "Fine Stripe Short Sleeve Shirt, Grey",
+              catagory: "Men’s Casualwear",
+              price: 49.00,
+              stock: 9,
+              img: "FineStripeShortSleeveShirtGrey.png")
+Product.create(name: "Fine Stripe Short Sleeve Shirt, Green",
+              catagory: "Men’s Casualwear",
+              price: 39.00,
+              stock: 3,
+              img: "FineStripeShortSleeveShirtGreen.png")
+Product.create(name: "Shark skin Waistcoat, Charcoal",
+              catagory: "Men’s Formalwear",
+              price: 75.00,
+              stock: 2,
+              img: "SharkskinWaistcoatCharcoal.png")
+Product.create(name: "Lightweight Patch Pocket Blazer, Deer",
+              catagory: "Men’s Formalwear",
+              price: 175.00,
+              stock: 1,
+              img: "LightweightPatchPocketBlazerDeer.png")
+Product.create(name: "Bird Print Dress, Black",
+              catagory: "Women’s Formalwear",
+              price: 270.00,
+              stock: 10,
+              img: "BirdPrintDressBlack.png")
+Product.create(name: "Mid-Twist Cut Out Dress, Pink",
+              catagory: "Women’s Formalwear",
+              price: 540.00,
+              stock: 5,
+              img:"MidTwistCutOutDressPink.png" )
